69a4c880c36f8c95d676a4945b35f21b6f78a841
rinvel7 <rinavelasquez03@gmail.com>
2023-04-18 20:24:39 -0400

    Update

--- menu.js
+++ menu.js
@@ -1,5 +1,6 @@
     // Seleciona o elemento de menu
     const menu = document.querySelector(".menu");
+    
 
     // Adiciona um listener para o evento "scroll"
     window.addEventListener("scroll", function() {
@@ -7,8 +8,20 @@
       if (window.pageYOffset > 100) {
         menu.classList.add("menu-scroll");
         menu.classList.add("menu-logo2");
+        trocaTitulo.style.color = "fff";
+            
       } else {
         menu.classList.remove("menu-scroll");
         menu.classList.remove("menu-logo2");
+      }
+    });
+
+    const navTitle = document.querySelector(".titulo_logo");
+
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        navTitle.style.color = "#fff";
+      } else {
+        navTitle.style.color = "#000";
       }
     });
